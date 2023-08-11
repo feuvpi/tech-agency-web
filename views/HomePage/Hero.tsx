af2a151e552751bddcb25d569b9b9491c3946209
feuvpi <fredvpg@gmail.com>
2023-08-10 23:53:18 -0300

    updating hero texts

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,12 +14,10 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>Impulsione seu negocio para o sucesso digital</CustomOverTitle>
+        <Heading>Delineando o futuro com tecnologia</Heading>
         <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
-          repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
-        </Description>
+        Bem-vindo à Kiwi Tech, onde transformamos ideias em realidade digital. Somos uma agência de tecnologia apaixonada por inovação, design elegante e soluções tecnológicas de alto desempenho, da concepção à implementação.</Description>
         <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
             Subscribe to the newsletter <span>&rarr;</span>
