a2639e0d5ef51ddd08c7a5493108529ca8c8466e
feuvpi <fredvpg@gmail.com>
2023-08-10 23:52:58 -0300

    updatig navbar props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,10 @@ import { NewsletterModalContextProvider, useNewsletterModalContext } from 'conte
 import { NavItems } from 'types';
 
 const navItems: NavItems = [
-  { title: 'Awesome SaaS Features', href: '/features' },
-  { title: 'Pricing', href: '/pricing' },
-  { title: 'Contact', href: '/contact' },
-  { title: 'Sign up', href: '/sign-up', outlined: true },
+  //{ title: 'Awesome SaaS Features', href: '/features' },
+  //{ title: 'Pricing', href: '/pricing' },
+  { title: 'Sobre', href: '/contact' },
+  { title: 'Contato', href: '/sign-up', outlined: true },
 ];
 
 const TinaCMS = dynamic(() => import('tinacms'), { ssr: false });
