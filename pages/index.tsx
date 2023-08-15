a6c055e87949a54871835412e125da035e883591
feuvpi <fredvpg@gmail.com>
2023-08-14 22:54:14 -0300

    updating index

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,25 +27,18 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           <Partners />
-          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
+          <BasicSection imageUrl="/demo-illustration-1.svg" title="Desenvolvimento de Aplicações Mobile" overTitle="Mobile">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-              voluptate quo deleniti animi laboriosam.{' '}
-              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
-              at suscipit quis est soluta?
+            Transformamos conceitos em experiências digitais. Criamos aplicativos e sites sob medida, garantindo usabilidade intuitiva e desempenho excepcional.{' '}
+              <Link href="/help-center">Veja nosso portfolio.</Link>
             </p>
           </BasicSection>
-          <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
+          <BasicSection imageUrl="/demo-illustration-2.svg" title="Conecte-se com IAs" overTitle="Inteligência Artificial" reversed>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
-              <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
+            Conectamos inteligência artificial ao seu ecossistema tecnológico. Nossa expertise em integração permite que seus sistemas e bancos de dados colaborem perfeitamente com modelos de AI,{' '}
+              <strong>impulsionando insights e eficiência.</strong>
             </p>
-            <ul>
-              <li>Professional point 1</li>
-              <li>Professional remark 2</li>
-              <li>Professional feature 3</li>
-            </ul>
+
           </BasicSection>
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
